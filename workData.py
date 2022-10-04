59aa27fa289fd8d4bf6264f63be8e3f1e9621e66
villi02 <villiarnar1@gmail.com>
2022-10-04 18:33:15 +0200

    Initial commit

---
+++ workData.py
@@ -0,0 +1,26 @@
+import matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fields = ['Landingstidspunkt', 'Bruttovekt']
+df = pd.read_csv('fangstdata_2022.csv', sep=';', error_bad_lines=False, usecols=fields)
+
+print("before", df['Bruttovekt'].size)
+
+df['Bruttovekt'] = df['Bruttovekt'].str.replace(',', '.').astype('float64')
+#df['Bruttovekt'] = df['Bruttovekt'].astype('float64')
+
+'''
+plt.plot(df['Bruttovekt'], label='caught', color='red')
+plt.xlabel("Car nr")
+plt.ylabel("Price of model3")
+plt.legend()
+'''
+#df["Bruttovekt"].plot(kind='hist')
+
+#plt.show()
+
+print("after", df['Bruttovekt'].size)
+print(df['Bruttovekt'])
+print(df)
