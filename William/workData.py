b100527244919cfe634fb75919184bd1fa7ce281
villi02 <villiarnar1@gmail.com>
2022-10-18 23:08:27 +0200

    fixed dataset

--- William/workData.py
+++ William/workData.py
@@ -2,19 +2,36 @@ import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 fields = ['Landingstidspunkt', 'Bruttovekt']
 df = pd.read_csv('fangstdata_2022.csv', sep=';', error_bad_lines=False, usecols=fields)
 
-print("before", df['Bruttovekt'].size)
+print("before", df.head())
 
 df['Bruttovekt'] = df['Bruttovekt'].str.replace(',', '.').astype('float64')
+
+
+n = len(pd.unique(df['Landingstidspunkt']))
+
+df = df.groupby(['Landingstidspunkt']).sum().reset_index()
+
+df['Landingstidspunkt'] = pd.to_datetime(df['Landingstidspunkt'], dayfirst=True)
+df = df.sort_values(by=['Landingstidspunkt'], ascending=True).
+df.sort_values(by='Landingstidspunkt',ascending=False)
+df.to_csv('output.csv', columns = fields)
+df.groupby(['Landingstidspunkt'])
+## Need to reset data
+
+#df['Landingstidspunkt'].dt.strftime('%m/%d/%Y')
+
+print("after", df)
+
+
+
 #df['Bruttovekt'] = df['Bruttovekt'].astype('float64')
 
-#df["Bruttovekt"].plot(kind='hist')
+#df.plot( 'Landingstidspunkt' , 'Bruttovekt' )
 
-#plt.show()
+print(n)
 
-print("after", df['Bruttovekt'].size)
-print(df['Bruttovekt'])
-print(df)
